5fd6bef97398e9920beb3a32bd5ef45ab2bd474c
gesaracino <gersaremail-github@yahoo.it>
2014-12-16 11:38:21 +0100

    Fixes on Exception Handling

--- src/main/java/com/gesaracino/gcm/boundary/EJBExceptionHandler.java
+++ src/main/java/com/gesaracino/gcm/boundary/EJBExceptionHandler.java
@@ -2,7 +2,9 @@ package com.gesaracino.gcm.boundary;
 
 import javax.ejb.EJBException;
 import javax.persistence.NoResultException;
+import javax.validation.ValidationException;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
@@ -14,8 +16,12 @@ import javax.ws.rs.ext.Provider;
 public class EJBExceptionHandler implements ExceptionMapper<EJBException> {
     @Override
     public Response toResponse(EJBException exception) {
-        if(exception.getCause() instanceof NoResultException) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("No result found: " + exception.getCause().getMessage()).build();
+    	Throwable cause = exception.getCause();
+    	
+        if(cause instanceof NoResultException) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("No result found: " + cause.getMessage()).build();
+        } else if(cause instanceof ValidationException) {
+        	return Response.status(Status.BAD_REQUEST).entity("Fill all fields").build();
         }
 
         return Response.status(Response.Status.BAD_REQUEST).entity("An error occurred while processing your request: " + exception.getCause().getMessage()).build();
